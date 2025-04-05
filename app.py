93842746c7c5c3132d75a93fa97ca7db44a59e06
Shiva Sai <57152818+Shiva-7@users.noreply.github.com>
2025-04-05 20:33:57 +0000

    Initial commit for Birthday Sparkle app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from datetime import datetime, date
+
+app = Flask(__name__)
+
+def calculate_age_and_days(dob):
+    today = date.today()
+    dob_date = datetime.strptime(dob, "%Y-%m-%d").date()
+    age = today.year - dob_date.year - ((today.month, today.day) < (dob_date.month, dob_date.day))
+
+    next_birthday = dob_date.replace(year=today.year)
+    if next_birthday < today:
+        next_birthday = next_birthday.replace(year=today.year + 1)
+    
+    days_remaining = (next_birthday - today).days
+    return age, days_remaining
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ""
+    if request.method == 'POST':
+        name = request.form['name']
+        gender = request.form['gender']
+        dob = request.form['dob']
+        age, days_remaining = calculate_age_and_days(dob)
+
+        if gender == 'Male':
+            message = f"Hey {name}, The Handsome Hunk you are {age} years old & you have {days_remaining} days remaining for your next birthday, Yay!"
+        else:
+            message = f"Hey {name}, Miss Beautiful you are {age} years old & you have {days_remaining} days remaining for your next birthday, Yay!"
+    
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
